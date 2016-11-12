11af189739e7c209138f5603135e01f872c20bdc
Mohamad Yuzrie Khalid <yuyieyuzrie@gmail.com>
2016-11-13 07:29:52 +0800

    _variables, app.scss - added list-groups. viewer homepage - tabbed display to switch between All(Home), Trending, Favourites

--- resources/views/viewer/home.blade.php
+++ resources/views/viewer/home.blade.php
@@ -2,6 +2,42 @@
 
 @section('content')
 <div class="container">
-    
-</div>
+    <div class="row">
+        {{-- Menu --}}
+        <div class="col-md-3">
+            <div class="list-group table-of-contents">
+                <a href="{{ url('/viewer') }}" class="list-group-item active">Home</a>
+                <a href="{{ url('/viewer/profile') }}" class="list-group-item">Profile</a>
+                <a href="{{ url('/viewer/channels') }}" class="list-group-item">Channels</a>
+                <a href="{{ url('/viewer/billing') }}" class="list-group-item">Billing</a>
+                <a href="{{ url('/viewer/faq') }}" class="list-group-item">FAQ</a>
+            </div>
+        </div>
+
+        {{-- Contents --}}
+        <div class="col-md-9">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <div>
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+                            <li role="presentation"><a href="#trending" aria-controls="trending" role="tab" data-toggle="tab">Trending</a></li>
+                            <li role="presentation"><a href="#favourites" aria-controls="favourites" role="tab" data-toggle="tab">Favourites</a></li>
+                        </ul>
+
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane active" id="home">Home</div>
+                            <div role="tabpanel" class="tab-pane" id="trending">Trending</div>
+                            <div role="tabpanel" class="tab-pane" id="favourites">Favourites</div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div> {{-- end .col --}}
+    </div> {{-- end .row --}}
+</div> {{-- end .container --}}
 @endsection
