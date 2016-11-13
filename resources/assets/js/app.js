bd1670baa6ba3de03a06a1287910e9d840e39471
Timur Kiyivinski <deathseclipse@gmail.com>
2016-11-13 10:43:23 +0800

    [ADD] JS Loader [REMOVE] PayPal

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,10 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * include Vue and Vue Resource. This gives a great starting point for
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -13,8 +12,13 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+;(function () {
+  const url = (window.location.pathname + window.location.search).substr(1)
 
-const app = new Vue({
-    el: '#app'
-});
+  try {
+    console.log(`Loading JS for ${url}`)
+    require(url)
+  } catch (e) {
+    console.log(`Unable to load ${url}`)
+  }
+})()
