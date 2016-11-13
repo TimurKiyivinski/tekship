005d2c7379d9fb8579dce2ecef91a4e75f8a945d
Timur Kiyivinski <deathseclipse@gmail.com>
2016-11-13 14:18:43 +0800

    [FIX] Registration redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/viewer';
 
     /**
      * Create a new controller instance.
